b0054b67749a535a54f13d6ce29dfbaf6e13bf32
mriart <marc_riart@es.ibm.com>
2022-11-28 16:11:49 +0100

    Recompile for url params

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ type Doc struct {
 
 // A hello function, to review the server is up and running.
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello2\n")
+	fmt.Fprintf(w, "Hello\n")
 }
 
 // Function to review the server is up and running, and well connected to Cloudant.
